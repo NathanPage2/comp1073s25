d71f983c54df37181482c9ea9384b0ae1883ad85
Nathan Page <200609542@student.georgianc.on.ca>
2025-05-26 11:35:34 -0400

    Created five button arrays, and two empty ones

---
+++ Assignment 1/js/say.js
@@ -0,0 +1,10 @@
+let button1opt = ["The Turkey", "Mom", "Dad", "The Dog", "My Teacher", "The Elephant", "The Cat" ];
+let button2opt = ["Sat On", "Ate", "Danced With", "Saw", "Doesn't Like", "Kissed" ];
+let button3opt = ["A Funny", "A Scary", "A Goofy", "A Slimy", "A Barking", "A Fat"];
+let button4opt = ["Goat", "Monkey", "Fish", "Cow", "Frog", "Bug", "Worm"];
+let button5opt = ["On The Moon", "On The Chair", "In My Spaghetti", "In My Soup", "On The Grass", "In My Shoes"];
+//We are simply just putting the options that the user can see into Arrays to use later
+
+let suprises = [];
+let playback = [];
+//Creating two empty arrays to use later (unsure if this is a good idea so far)
