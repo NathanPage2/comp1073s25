380f33c70d312b4ec15a4e2b1497d663e4c97126
Nathan Page <200609542@student.georgianc.on.ca>
2025-07-10 11:27:47 -0400

    I wanted more a of a space with the button on the html and added a constructor and class to js

---
+++ Assignment 2/js/smoothie.js
@@ -0,0 +1,16 @@
+//Done by Nathan Page (200609542)
+//June 10th, 2025
+
+const output = document.getElementById("order");
+
+class Smoothie{
+    name;
+    flavor;
+    size;
+    constructor(name, flavor, size){
+        this.name = name;
+        this.flavor = flavor;
+        this.size = size;
+    }
+    
+}
